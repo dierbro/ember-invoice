97ba02c54a9b629df43bf205c9048c58a21bfcc9
Diego R. Brogna <dierbro@gmail.com>
2013-08-22 18:42:13 +0200

    Add routeFor test helper

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -5,8 +5,6 @@ Ember.testing = true;
 var App = requireModule('appkit/app');
 
 App.rootElement = '#ember-testing';
-App.setupForTesting();
-App.injectTestHelpers();
 
 function exists(selector) {
   return !!find(selector).length;
@@ -24,3 +22,11 @@ Ember.Container.prototype.stub = function(fullName, instance) {
   instance.destroy = instance.destroy || function() {};
   this.cache.dict[fullName] = instance;
 };
+
+// https://github.com/stefanpenner/ember-app-kit/pull/80
+Ember.Test.registerHelper('routeFor', function(app, routeName) {
+  return app.__container__.lookup('route:' + routeName);
+});
+
+App.setupForTesting();
+App.injectTestHelpers();
