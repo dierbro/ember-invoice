b745a4494002a730e390bc346bd99f090c69aba0
Diego R. Brogna <dierbro@gmail.com>
2013-08-28 07:53:44 +0200

    Fix jshint warnings

--- app/views/in_place_field.js
+++ app/views/in_place_field.js
@@ -7,26 +7,26 @@ var InPlaceFieldView = Ember.View.extend({
   isEditing: false,
   layoutName: "in_place_edit",
   templateName: function(){
-    if(this.get("contentType") == 'currency'){
-      return 'in_place_currency_field'
+    if(this.get("contentType") === 'currency'){
+      return 'in_place_currency_field';
     } else {
       return 'in_place_text_field';
     }
   }.property(),
   isEmpty: function(){
-    return Ember.isEmpty(this.get('content'))
+    return Ember.isEmpty(this.get('content'));
   }.property('content'),
   
   inputField: Ember.TextField.extend({}),
   emptyValue: "Click to Edit",
   focusOut: function(){
     this.get('controller').get('store').commit();
-    this.set('isEditing', false)
+    this.set('isEditing', false);
   },
 
   click: function(){
     this.set("isEditing", true);
   }
-})
+});
 
 export default InPlaceFieldView;
